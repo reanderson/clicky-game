9ff419762a7d113c6e4d83b04db1e15750a5f290
Tasima <re.anderson617@gmail.com>
2018-11-13 12:16:45 -0500

    Updates

---
+++ precache-manifest.da06d2089bfd1233270e36296d394017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f305602e293abb081b3a",
+    "url": "/static/js/main.f305602e.chunk.js"
+  },
+  {
+    "revision": "81c887800f04c9e95282",
+    "url": "/static/js/1.81c88780.chunk.js"
+  },
+  {
+    "revision": "f305602e293abb081b3a",
+    "url": "/static/css/main.1cc2ca40.chunk.css"
+  },
+  {
+    "revision": "85898a5c25864597750530d1bb73e15c",
+    "url": "/index.html"
+  }
+];
